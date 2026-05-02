6f996929521c1891928c1daf4ece4c507e1eea28
Fabrizio Meschini <fmmeschini@gmail.com>
2026-05-02 15:36:56 -0300

    Agregada solución al problema de parcelas con O(1) espacio.

--- Dinamic Programming/parcels.py
+++ Dinamic Programming/parcels.py
@@ -12,5 +12,19 @@ def solution(C):
         min_cost = costs[n-1]
     return min_cost
 
+def solution_opt(C):
+    n = len(C)
+    min_cost = None
+    if n < 2:
+        min_cost = C[0]
+    else:
+        prev_1 = C[1]
+        prev_2 = C[0]
+        min_cost = min(prev_1, prev_2)
+        for i in range(2, n):
+            min_cost = C[i] + min(prev_1, prev_2)
+            prev_1, prev_2 = min_cost, prev_1
+    return min_cost
+
 parcels_costs = [10, 5, 12, 1, 1, 20, 12, 7, 3]
-print(solution(parcels_costs))
+print(solution_opt(parcels_costs))
